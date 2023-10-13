431cbddfda4fa902eafccb79647437d068cd7f91
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-13 13:41:52 -0600

    react navigation setup

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
-      <StatusBar backgroundColor="black" barStyle="light-content" />
-      <View>
-        <Text style={styles.text}>Hello World!</Text>
+    <NavigationContainer>
+      <View style={styles.backgroundStyle}>
+        <Text style={styles.text}>Hello World!a</Text>
       </View>
-    </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
