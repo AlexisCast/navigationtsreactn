ffcddc7ba14d09e4f0d54195b913d8dfdf14619e
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-25 23:07:04 -0600

    personalized material top tab

--- src/navigator/TopTabNavigator.tsx
+++ src/navigator/TopTabNavigator.tsx
@@ -5,14 +5,45 @@ import {ChatScreen} from '../screens/ChatScreen';
 import {ContactsScreen} from '../screens/ContactsScreen';
 import {AlbumsScreen} from '../screens/AlbumsScreen';
 
-import {LogBox} from 'react-native';
+import {LogBox, Text} from 'react-native';
+import {Colors} from '../theme/appThemes';
 LogBox.ignoreLogs(['Sending']);
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      screenOptions={({route}) => ({
+        tabBarPressColor: Colors.primary,
+        tabBarStyle: {
+          shadowColor: 'transparent',
+          elevation: 0,
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: Colors.primary,
+        },
+        tabBarIcon: ({color, focused}) => {
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Chat':
+              iconName = 'Ch';
+              break;
+
+            case 'Contacts':
+              iconName = 'Co';
+              break;
+
+            case 'Albums':
+              iconName = 'Al';
+              break;
+          }
+          return <Text style={{color: Colors.primary}}>{iconName}</Text>;
+        },
+      })}>
       <Tab.Screen name="Chat" component={ChatScreen} />
       <Tab.Screen name="Contacts" component={ContactsScreen} />
       <Tab.Screen name="Albums" component={AlbumsScreen} />
