412f60de121c94d0daa136e09f93f7b8d60fa7ca
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-26 00:24:30 -0600

    setup Icons for IOS

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,7 +1,8 @@
 import React, {useEffect} from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {Text, View} from 'react-native';
-import {styles} from '../theme/appThemes';
+import {Colors, styles} from '../theme/appThemes';
+
 export const Tab1Screen = () => {
   useEffect(() => {
     console.log('Tab1Screen effect');
@@ -11,7 +12,11 @@ export const Tab1Screen = () => {
     <View style={styles.globalMargin}>
       <Text style={styles.title}>Icons</Text>
       <Text>
-        <Icon name="airplane-outline" size={50} color="#900" />;
+        <Icon name="airplane-outline" size={50} color={Colors.primary} />
+        <Icon name="accessibility-outline" size={50} color={Colors.primary} />
+        <Icon name="add-outline" size={50} color={Colors.primary} />
+        <Icon name="arrow-back-outline" size={50} color={Colors.primary} />
+        <Icon name="brush-outline" size={50} color={Colors.primary} />
       </Text>
     </View>
   );
