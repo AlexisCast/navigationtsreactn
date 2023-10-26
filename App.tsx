97b85b8023aebfac049805bffceb1d629fdcf50a
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-26 01:45:32 -0600

    setup Context

--- App.tsx
+++ App.tsx
@@ -5,17 +5,24 @@ import {NavigationContainer} from '@react-navigation/native';
 // import {StackNavigator} from './src/navigator/StackNavigator';
 // import {LateralBasicMenu} from './src/navigator/LateralBasicMenu';
 import {LateralMenu} from './src/navigator/LateralMenu';
+import {AuthProvider} from './src/context/authContext';
 // import {Tabs} from './src/navigator/Tabs';
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/* <StackNavigator /> */}
-      {/* <LateralBasicMenu /> */}
-      <LateralMenu />
-      {/* <Tabs /> */}
+      <AppState>
+        {/* <StackNavigator /> */}
+        {/* <LateralBasicMenu /> */}
+        <LateralMenu />
+        {/* <Tabs /> */}
+      </AppState>
     </NavigationContainer>
   );
+};
+
+const AppState = ({children}: any) => {
+  return <AuthProvider>{children}</AuthProvider>;
 };
 
 export default App;
